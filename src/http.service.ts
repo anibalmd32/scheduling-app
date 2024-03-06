147dee629a8a181c92a98486507efffd770ac2c3
Anibal Mendoza <anibalmd32@gmail.com>
2024-03-06 06:08:46 -0400

    http service

---
+++ src/http.service.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
+const API_URL = 'http://localhost:8080/api';
+
+interface API {
+	endpoint: string;
+	method: 'get' | 'post' | 'put' | 'delete';
+	params?: {
+		id?: string;
+	},
+	body?: unknown;
+}
+
+class HTTPService {
+	private callerInstance() {
+		const axiosInstance: AxiosInstance = axios.create({
+			baseURL: API_URL
+		});
+
+		return axiosInstance;
+	}
+
+	private requestInterceptor(instance: AxiosInstance) {
+		instance.interceptors.request.use((config) => {
+			config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+
+			return config;
+		}, (err) => {
+			return Promise.reject(err);
+		});
+	}
+
+	async httpCaller<T>(props: API): Promise<AxiosResponse<T>> {
+		const { endpoint, method, body, params } = props;
+		const instace = this.callerInstance();
+		const config: AxiosRequestConfig = {
+			method,
+			params,
+			data: body
+		};
+
+		this.requestInterceptor(instace);
+
+		const res = await instace(endpoint, config);
+
+		return res;
+	}
+}
+
+export default HTTPService;
